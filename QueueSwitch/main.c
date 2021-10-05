962beabd5cee32b310ca6f51e1c86414fce1499c
Kavprabath <kavinduprabath7@gmail.com>
2021-10-05 21:54:30 +0530

    new methods added

---
+++ QueueSwitch/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 50
+
+void insert();
+void delete();
+void display();
+int queue_array[MAX];
+int rear = - 1;
+int front = - 1;
+main()
+{
+    int choice;
+    while (1)
+    {
+        printf("1.Insert elements to queue \n");
+        printf("2.Delete element from queue \n");
+        printf("3.Display all elements of queue \n");
+        printf("4.Quit \n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+            insert();
+            break;
+            case 2:
+            delete();
+            break;
+            case 3:
+            display();
+            break;
+            case 4:
+            exit(1);
+            default:
+            printf("Wrong choice \n");
+        } /* End of switch */
+    } /* End of while */
+} /* End of main() */
+
+void insert()
+{
+    int add_item;
+    int noofeliments;
+    int x;
+    if (rear == MAX - 1)
+    printf("Queue Overflow \n");
+    else
+    {
+        if (front == - 1)
+        /*If queue is initially empty */
+        front = 0;
+        printf("How many elements should be inserted in  the queue : ");
+        scanf("%d", &noofeliments);
+        for(x=0;x<noofeliments;x++){
+            printf("Enter number %d : ",x+1);
+            scanf("%d",&add_item);
+            queue_array[x] = add_item;
+            rear= rear + 1;
+        }
+
+
+    }
+} /* End of insert() */
+
+void delete()
+{
+    if (front == - 1 || front > rear)
+    {
+        printf("Queue Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from queue is : %d\n", queue_array[front]);
+        front= front + 1;
+    }
+} /* End of delete() */
+
+void display()
+{
+    int i;
+    if (front == - 1)
+        printf("Queue is empty \n");
+    else
+    {
+        printf("Queue is : \n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+    }
+} /* End of display() */
